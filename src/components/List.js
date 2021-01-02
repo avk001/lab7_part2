7264c78f5b9cde66817ed266b8d99aa247e31ac2
avk001 <avk001@yandex.ru>
2021-01-02 17:38:57 +0300

    update

--- src/components/List.js
+++ src/components/List.js
@@ -1,34 +1,31 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+
 import Article from './Article'
 import Video from './Video'
 import Popular from './Popular';
 import New from './New';
 
+import WrappedComponent from './WrappedComponent'
+
+const SimpleHOC = WrappedComponent(New, Popular, 1000 );
+
 function List(props) {
     return props.list.map(item => {
         switch (item.type) {
             case 'video':
-                if(item.views >=1000) {
-                    return(
-                        <Popular children={<Video {...item} />}/>
-                    )
-                } else {
-                    return (
-                        <New children={<Video {...item} />}/>
-                    )
-                }
+                return <SimpleHOC  children={<Video {...item} />} views={item.views} />
 
             case 'article':
-                if(item.views >=1000) {
-                    return(
-                        <Popular children={<Article {...item} />}/>
-                    )
-                } else {
-                    return (
-                        <New children={<Article {...item} />}/>
-                    )
-                }
+                return <SimpleHOC  children={<Article {...item} />}  views={item.views}  />
+                // if(item.views >=1000) {
+                //     return(
+                //         <Popular children={<Article {...item} />}/>
+                //     )
+                // } else {
+                //     return (
+                //         <New children={<Article {...item} />}/>
+                //     )
+                // }
         }
     });
 };
